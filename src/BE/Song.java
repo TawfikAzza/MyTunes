37b653aa54d882e6594620aa9dcf647f9553b96e
TawfikAzza <tawf0021@easv365.dk>
2021-11-29 20:12:13 +0100

    Changed the song class a little to accomodate the file management (this is a test for now)

--- src/BE/Song.java
+++ src/BE/Song.java
@@ -3,7 +3,11 @@ package BE;
 import DAL.AuthorDAO;
 import javafx.scene.media.Media;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 
@@ -13,6 +17,7 @@ public class Song {
     private Author author;
     private CategorySong category;
     private Media songFile;
+    private static final String PATH_TO_FILE = "C:/Users/EASV/Desktop/SCO/MyTunes/resources/";
     public Song(int id, String name, Author author, CategorySong category) {
         this.id = id;
         this.name = name;
@@ -52,6 +57,11 @@ public class Song {
     public Media getSongFile() {
         return songFile;
     }
-//    public setSong
+    public void setSongFile(File file) throws IOException {
+        Path src = Paths.get(file.getAbsolutePath());
+        Path dest = Paths.get(PATH_TO_FILE + file.getName().toString());
+        Files.copy(src, dest);
+        songFile = new Media(PATH_TO_FILE + category.getName() + "/" + file.getName());
+    }
 
 }
