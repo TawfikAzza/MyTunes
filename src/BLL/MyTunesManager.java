056b1b0eb6db1bdb8f73e90767eb2c85d9030880
Ladam0203 <adam7605@easv365.dk>
2021-11-30 10:24:40 +0100

    Made author dao methods avaliable through manager

--- src/BLL/MyTunesManager.java
+++ src/BLL/MyTunesManager.java
@@ -1,18 +1,49 @@
 package BLL;
 
+import BE.Author;
 import BLL.util.SongPlayer;
 import DAL.DB.AuthorDAO;
 import DAL.DB.CategoryDAO;
+import DAL.DB.PlayListDAO;
+import DAL.DB.SongDAO;
 
 import java.io.IOException;
+import java.util.List;
 
 public class MyTunesManager {
     SongPlayer songPlayer;
 
     AuthorDAO authorDAO;
     CategoryDAO categoryDAO;
+    PlayListDAO playListDAO;
+    SongDAO songDAO;
 
     public MyTunesManager() throws IOException {
+        songPlayer = new SongPlayer();
+
         authorDAO = new AuthorDAO();
+        categoryDAO = new CategoryDAO();
+        playListDAO = new PlayListDAO();
+        songDAO = new SongDAO();
     }
+
+    public Author getAuthor(int id) {
+        return authorDAO.getAuthor(id);
+    }
+
+    public List<Author> getALlAuthors() {
+        return authorDAO.getALlAuthors();
+    }
+
+    public Author createAuthor(String name) {
+        return authorDAO.createAuthor(name);
+    }
+
+    public void updateAuthor(Author author) {
+        authorDAO.updateAuthor(author);
+    }
+    public void deleteAuthor(Author author) {
+        authorDAO.updateAuthor(author);
+    }
+
 }
