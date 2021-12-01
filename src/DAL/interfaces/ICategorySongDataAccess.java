6a5715f2e8e4bc2a7245acce8ad531599885b4ff
TawfikAzza <tawf0021@easv365.dk>
2021-12-01 10:58:04 +0100

    Added toString in PlayList Object

--- src/DAL/interfaces/ICategorySongDataAccess.java
+++ src/DAL/interfaces/ICategorySongDataAccess.java
@@ -17,9 +17,20 @@ public interface ICategorySongDataAccess {
      * */
     List<CategorySong> getALlCategorySong() throws Exception;
     /**
-     *
+     * Create a category with the name specified in the Category object
+     * Return a CategorySong object containing the ID retrieved from
+     * the database after the insert
      * */
     CategorySong createCategorySong(CategorySong category) throws Exception;
+
+    /**
+     * Update the name of the category
+     * Take a CategorySong object as argument
+     * */
     void updateCategorySong(CategorySong categorySong) throws Exception;
+    /**
+     * Delete a Category
+     * returns nothing
+     * */
     void deleteCategorySong(CategorySong categorySong) throws Exception;
 }
--- src/DAL/interfaces/IPlayListDataAccess.java
+++ src/DAL/interfaces/IPlayListDataAccess.java
@@ -5,7 +5,12 @@ import BE.PlayList;
 import java.util.List;
 
 public interface IPlayListDataAccess {
+    /**
+     * Returns a Playlist with the specified ID.
+     * See Also (PlayList Object to know what is returned)
+     * */
     PlayList getPlayList(int idPlayList) throws Exception;
+
     List<PlayList> getALlPlayLists() throws Exception;
     PlayList createPlayList(PlayList playList) throws Exception;
     void updatePlayList(PlayList playList) throws Exception;
